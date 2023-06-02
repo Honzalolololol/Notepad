ca720b2b32a821c356c4a5903ce64249001d64b0
Honzalolololol <jan.richter@student.spsezatec.cz>
2023-06-02 09:52:00 +0200

    Co-authored-by: Kůrka Matěj <Matej.Kurka@student.spsezatec.cz>

--- Main.java
+++ Main.java
@@ -1,5 +1,13 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 public class Main {
     public static void main(String[] args) {
-        new Notepad();
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                new Notepad();
+            }
+        });
+    
     }
 }
--- Notepad.java
+++ Notepad.java
@@ -4,15 +4,17 @@ import java.io.IOException;
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.FileWriter;
+import java.util.Scanner;
 public class Notepad implements ActionListener{
 
     JFrame frame = new JFrame("Notepad");
-    JTextArea t1 = new JTextArea("");
+    static JTextArea t1 = new JTextArea("");
 
     JMenuBar mb = new JMenuBar();  
-    JMenu openBtn = new JMenu("Open");
-    JMenu newBtn = new JMenu("New");
-    JMenu saveBtn = new JMenu("Save");
+    JMenuItem openBtn = new JMenuItem("Open");
+    JMenuItem newBtn = new JMenuItem("New");
+    JMenuItem saveBtn = new JMenuItem("Save");
 
     Notepad(){
         prepareGUI();
@@ -39,24 +41,56 @@ public class Notepad implements ActionListener{
         mb.add(newBtn);
         mb.add(saveBtn);
         saveBtn.addActionListener(this);
+        openBtn.addActionListener(new ActionListener() {
+        public void actionPerformed(ActionEvent e) {
+                open();
+            }
+        });
     }
 
     public void actionPerformed(ActionEvent e){
-        
-    }
-
-    static void save(){
-        try {
-            File myObj = new File("filename.txt");
-            if (myObj.createNewFile()) {
-            System.out.println("File created: " + myObj.getName());
-            }else {
-            System.out.println("File already exists.");
-            }
-        } catch (IOException e) {
-        System.out.println("An error occurred.");
-        e.printStackTrace();
+        if (e.getSource() == saveBtn) {
+            save();
         }
     }
+
+    private void open(){
+        JFileChooser fileChooser = new JFileChooser();
+        int option = fileChooser.showOpenDialog(frame);
+        if (option == JFileChooser.APPROVE_OPTION) {
+        File file = fileChooser.getSelectedFile();
         
+        try {
+            Scanner scanner = new Scanner(file);
+            StringBuilder content = new StringBuilder();
+            while (scanner.hasNextLine()) {
+                content.append(scanner.nextLine()).append("\n");
+            }
+            scanner.close();
+            t1.setText(content.toString());
+            System.out.println("File opened: " + file.getName());
+        } catch (IOException e) {
+            System.out.println("An error occurred while opening the file.");
+            e.printStackTrace();
+        }
+    }
+    }
+
+    private void save(){
+        JFileChooser fileChooser = new JFileChooser();
+        int option = fileChooser.showSaveDialog(frame);
+        if (option == JFileChooser.APPROVE_OPTION) {
+            File file = fileChooser.getSelectedFile();
+            String filePath = file.getAbsolutePath();
+            
+            try (FileWriter writer = new FileWriter(filePath + ".txt")) {
+                writer.write(t1.getText());
+                System.out.println("File saved: " + filePath);
+            } catch (IOException e) {
+                e.printStackTrace();
+    
+            }
+    }   }
 }
+
+
